f70a06bd7afc861502b0c0ff1afec9a2826a0c89
Senay <senaialema@gmail.com>
2022-05-28 19:59:18 -0400

    implement takePhoto()

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -31,4 +31,20 @@ function paintToCanvas(){
 } 
 // paintToCanvas();
 
+function takePhoto() {
+    //played the sound
+    snap.currentTime = 0;
+    snap.play();
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`; 
+    strip.insertBefore(link, strip.firstChild);
+
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
